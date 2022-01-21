c806a1c61717e16b289b851dd204610dde814ac7
Cosinato <cosinato@pm.me>
2022-01-21 12:12:23 -0500

    reformatted

--- features/steps/hello.py
+++ features/steps/hello.py
@@ -2,16 +2,19 @@ import os
 from behave import given, when, then, step
 from subprocess import getstatusoutput, getoutput
 
-scriptFile = 'picklerick.py'
+scriptFile = "picklerick.py"
 
-@given('we have the script')
+
+@given("we have the script")
 def step_impl(context):
     assert os.path.exists(scriptFile)
 
-@when('we run the hello command')
-def step_impl(context ):
-    context.output = getoutput(f'python3 {scriptFile} hello')
 
-@then('we are greeted')
+@when("we run the hello command")
+def step_impl(context):
+    context.output = getoutput(f"python3 {scriptFile} hello")
+
+
+@then("we are greeted")
 def step_impl(context):
     assert context.output == "Hello, World"
--- tests/test_klickbrick.py
+++ tests/test_klickbrick.py
@@ -4,13 +4,12 @@ import os
 import unittest
 from picklerick import __version__
 
-targetProgram = 'picklerick.py'
+targetProgram = "picklerick.py"
 
 
 class TestPicklerickery(unittest.TestCase):
-
     def test_version(self):
-        assert __version__ == '0.1.0'
+        assert __version__ == "0.1.0"
 
     def test_program_exists(self):
         assert os.path.isfile(targetProgram)
